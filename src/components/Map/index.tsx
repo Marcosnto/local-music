deebc42ede9a399ce5aeda8ce15f56d84dfb5ea8
Marcos Neto <marcos_nto@hotmail.com>
2021-03-27 17:01:50 -0300

    make event click for marker

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/dist/client/router';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 type Artist = {
@@ -14,27 +15,36 @@ export type MapProps = {
   artists?: Artist[];
 };
 
-const Map = ({ artists }: MapProps) => (
-  <MapContainer
-    center={[51.505, -0.09]}
-    zoom={3}
-    style={{ height: '100%', width: '100%' }}
-  >
-    <TileLayer
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    {artists?.map(({ id, name, location }) => {
-      const { latitude, longitude } = location;
-      return (
-        <Marker
-          key={`place-${id}`}
-          title={name}
-          position={[latitude, longitude]}
-        />
-      );
-    })}
-  </MapContainer>
-);
+const Map = ({ artists }: MapProps) => {
+  const router = useRouter();
+
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={3}
+      style={{ height: '100%', width: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {artists?.map(({ id, slug, name, location }) => {
+        const { latitude, longitude } = location;
+        return (
+          <Marker
+            key={`place-${id}`}
+            title={name}
+            position={[latitude, longitude]}
+            eventHandlers={{
+              click: () => {
+                router.push(`/artists/${slug}`);
+              }
+            }}
+          />
+        );
+      })}
+    </MapContainer>
+  );
+};
 
 export default Map;
